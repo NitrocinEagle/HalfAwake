3f5883e5c0b958a55bd794e77574504523a8f1f2
der7leo <der7leo@gmail.com>
2013-07-01 12:37:57 +0800

    Added Map Class.

---
+++ HalfAwake/src/Game/Map.java
@@ -0,0 +1,23 @@
+package Game;
+
+import java.util.Set;
+
+public class Map {
+	
+	Set <MapObject> objects;
+	
+	boolean addObject(MapObject object)
+	{
+		return objects.add(object);
+	}
+	
+	Map(int widith, int height)
+	{
+		
+	}
+	
+	void draw()
+	{
+		
+	}
+}
