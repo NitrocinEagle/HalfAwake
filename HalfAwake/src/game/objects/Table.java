3ee89b48fb7e5e046c6156c5542a51b8fe2dac7e
der7leo <der7leo@gmail.com>
2013-07-03 17:59:17 +0800

    Created two new packages: objects and personages. Added two test classes in this packages: Hero and Table

---
+++ HalfAwake/src/game/objects/Table.java
@@ -0,0 +1,13 @@
+package game.objects;
+
+import game.MapObject;
+
+import java.awt.Image;
+
+public class Table extends MapObject {
+
+	public Table(Image image, int x, int y) {
+		super(image, x, y);
+	}
+
+}
---
+++ HalfAwake/src/game/personages/Hero.java
@@ -0,0 +1,13 @@
+package game.personages;
+
+import game.MapObject;
+
+import java.awt.Image;
+
+public class Hero extends MapObject {
+
+	public Hero(Image image, int x, int y) {
+		super(image, x, y);
+	}
+	
+}
