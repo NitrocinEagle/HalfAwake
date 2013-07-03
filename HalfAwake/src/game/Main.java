896b9671c4a750172f569bf023a0730832cf0f7f
der7leo <der7leo@gmail.com>
2013-07-03 18:43:44 +0800

    Now it's runnable :)

--- HalfAwake/src/game/Main.java
+++ HalfAwake/src/game/Main.java
@@ -1,9 +1,22 @@
 package game;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 public class Main {
 	public static void main(String args[]) {
+		Game game = new Game();
+		game.setPreferredSize(new Dimension(500, 400));
 		
+		JFrame frame = new JFrame("Half Awake 2D");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(game, BorderLayout.CENTER);
 		
+		frame.pack();
+		frame.setResizable(false);
+		frame.setVisible(true);
+		game.start();
 	}
-
 }
